f8767bd2b1102732b0e3067109e7d50b5af39fb0
lunou <lunou@tcl.com>
2018-04-23 14:50:15 +0800

    添加隐私政策多语言

--- privacy_policy_sdk/src/main/java/com/tcl/faext/PrivacyPolicyDialog.java
+++ privacy_policy_sdk/src/main/java/com/tcl/faext/PrivacyPolicyDialog.java
@@ -3,6 +3,7 @@ package com.tcl.faext;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.res.Resources;
+import android.text.Html;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
@@ -25,8 +26,21 @@ public class PrivacyPolicyDialog extends Dialog {
 
         TextView titleView = (TextView) findViewById(R.id.dialog_title);
         TextView contentView = (TextView) findViewById(R.id.dialog_content);
-        titleView.setText(context.getString(R.string.privacy_policy_dialog_informed_title));
+        titleView.setText(context.getString(R.string.policy) +
+                        context.getString(R.string.and) +
+                context.getString(R.string.tos));
 
+        init2(context, contentView);
+
+        findViewById(R.id.ok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+    private void init(final Context context, TextView contentView) {
         Resources res = context.getResources();
         String privacyText = res.getString(R.string.privacy);
         String serviceText = res.getString(R.string.service_terms);
@@ -74,13 +88,38 @@ public class PrivacyPolicyDialog extends Dialog {
         spannableStringBuilder.append(msg3);
         contentView.setText(spannableStringBuilder);
         contentView.setMovementMethod(LinkMovementMethod.getInstance());
+    }
 
-        findViewById(R.id.ok).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dismiss();
-            }
-        });
+    private void init2(final Context context, TextView contentView) {
+        Resources res = context.getResources();
+        String str1 = res.getString(R.string.privacy_policy_0_1);
+        String str2 = res.getString(R.string.privacy_policy_0_2);
+        String str3 = res.getString(R.string.privacy_policy_0_3);
+        String str4 = res.getString(R.string.privacy_policy_1_1);
+        String str5 = res.getString(R.string.privacy_policy_2_1);
+        String str6 = res.getString(R.string.privacy_policy_3_1);
+        String str7 = res.getString(R.string.privacy_policy_4_1);
+        String str8 = res.getString(R.string.privacy_policy_4_2);
+        String str9 = res.getString(R.string.privacy_policy_5_1);
+        String str10 = res.getString(R.string.privacy_policy_6_1);
+        String str11 = res.getString(R.string.privacy_policy_7_1);
+        String str12 = res.getString(R.string.privacy_policy_8_1);
+        String str13 = res.getString(R.string.privacy_policy_9_1);
+        String str14 = res.getString(R.string.privacy_policy_10_1);
+        String str15 = res.getString(R.string.privacy_policy_10_2);
+        String str16 = res.getString(R.string.privacy_policy_10_3);
+        String str17 = res.getString(R.string.privacy_policy_10_4);
+        String str18 = res.getString(R.string.privacy_policy_10_5);
+        String str19 = res.getString(R.string.privacy_policy_11_1);
+        String str20 = res.getString(R.string.privacy_policy_12_1);
+        String str21 = res.getString(R.string.privacy_policy_13_1);
+        String str22 = res.getString(R.string.privacy_policy_13_2);
+        String str23 = res.getString(R.string.privacy_policy_13_3);
+        Spanned spanned = Html.fromHtml(str1 + str2 + str3 + str4 + str5 + str6 + str7 +
+                str8 + str9 + str10 + str11 + str12 + str13 + str14 + str15 +
+                str16 + str17 + str18 + str19 + str20 + str21 + str22 + str23);
+        contentView.setText(spanned);
+        contentView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
